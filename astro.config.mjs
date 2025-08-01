8a9b9b053543dacc8bca936d99e797a11d0a3805
Dave Beach <hello@davebeach.me>
2025-08-01 19:45:11 -0400

    chore(repo): :arrow_up: upgrade Astro Vercel plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
-
 import icon from 'astro-icon';
-
 import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
+  output: 'server',
   integrations: [react(), icon()],
-  adapter: vercel()
+  adapter: vercel(),
 });
