5fe647fb4dc1cdf6d94d4df8ab03c358373035e5
Dave Beach <hello@davebeach.me>
2026-02-09 22:43:39 -0500

    feat(about): :lipstick: add polish to About section

--- src/components/home/about/Frameworks.jsx
+++ src/components/home/about/Frameworks.jsx
@@ -1,26 +1,36 @@
+import { OrbitingCircles } from './OrbitingCircles';
+
 export function Frameworks() {
   const skills = [
     'css3',
-    'git',
-    'html5',
+    'github',
     'javascript',
+    'threejs',
+    'python',
+    'qgis',
+    'esri',
+    'unreal',
     'react',
-    'sqlite',
+    'postgresql',
     'tailwindcss',
     'wordpress',
   ];
   return (
-    <div className="relative flex w-full">
-      {/* <OrbitingCircles iconSize={40}>
+    <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
+      <OrbitingCircles iconSize={40}>
         {skills.map((skill, index) => (
-          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+          <Icon key={index} src={`src/assets/about/logos/${skill}.svg`} />
         ))}
       </OrbitingCircles>
       <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
         {skills.reverse().map((skill, index) => (
-          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+          <Icon key={index} src={`src/assets/about/logos/${skill}.svg`} />
         ))}
-      </OrbitingCircles> */}
+      </OrbitingCircles>
     </div>
   );
 }
+
+const Icon = ({ src }) => (
+  <img src={src} className="duration-200 rounded-sm hover:scale-110" />
+);
