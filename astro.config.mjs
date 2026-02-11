0afa0957319cf561b9e0a7928bc2ecbd6dde7221
Dave Beach <hello@davebeach.me>
2026-02-11 12:32:44 -0500

    feat(repo): 📈 add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,21 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import icon from 'astro-icon';
 import vercel from '@astrojs/vercel';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss(), glsl()],
   },
-  integrations: [react(), icon()],
+  integrations: [
+    react(),
+    icon(),
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+    }),
+  ],
   adapter: vercel(),
 });
